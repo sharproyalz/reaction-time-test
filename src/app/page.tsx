633cedd6c72e83f493dafd2299585f78f927c6de
SharpRoyalz <116153953+SharpRoyalz@users.noreply.github.com>
2024-01-04 22:20:38 +0800

    Initial version of this web page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,120 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { useEffect, useState } from "react";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default function HomePage() {
+    // const box = document.getElementById("box");
+    // const textChange = document.getElementById("text-change");
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const randomNumber = Math.random() * 3000 + 3000;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const [start, setStart] = useState(true);
+    const [isStarted, setIsStarted] = useState(false);
+    const [timeRemaining, setTimeRemaining] = useState(0);
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    const startTimer = () => {
+        console.log("Clicked");
+        console.log("Time is: ", randomNumber);
+        setTimeRemaining(0);
+        setStart(true);
+        setTimeout(() => {
+            setIsTimerRunning(true);
+            setStart(false);
+        }, randomNumber);
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    useEffect(() => {
+        if (isTimerRunning && timeRemaining < 5) {
+            const intervalId = setInterval(() => {
+                setTimeRemaining((prevTime) => prevTime + 0.01);
+                console.log(timeRemaining);
+            }, 10);
+
+            return () => clearInterval(intervalId);
+        }
+    }, [isTimerRunning, timeRemaining]);
+
+    return (
+        <>
+            <div className="flex flex-col items-center gap-4 h-[100vh] justify-center bg-slate-900">
+                {start ? (
+                    <button
+                        type="button"
+                        onClick={() => {
+                            startTimer();
+                            setIsStarted(true);
+                        }}
+                        className={`bg-white w-96 h-96 text-2xl font-bold rounded-sm`}
+                    >
+                        {!isStarted && "Click me to get started"}
+                    </button>
+                ) : (
+                    <button
+                        type="button"
+                        onClick={() => {
+                            setIsTimerRunning(false);
+                            setShowModal(true);
+                            console.log(
+                                "You clicked the button",
+                                timeRemaining.toString().slice(0, 3),
+                                "Seconds"
+                            );
+                        }}
+                        className={`bg-red-500 w-96 h-96  rounded-sm text-2xl text-white font-bold`}
+                    ></button>
+                )}
+
+                <div className="text-4xl text-white font-bold">
+                    {isStarted && start ? (
+                        <div>
+                            Click the box when it turns{" "}
+                            <span className="text-red-500"> red</span>, click
+                            the box!
+                        </div>
+                    ) : !start ? (
+                        "Click now"
+                    ) : (
+                        ""
+                    )}
+                </div>
+            </div>
+            <div
+                className={`${
+                    showModal
+                        ? "opacity-100 duration-500 transition-all translate-y-4"
+                        : "opacity-0"
+                } absolute top-2 translate-y-0 left-[35%] p-4 rounded-sm bg-white `}
+            >
+                <div className="text-2xl font-medium">
+                    You clicked the button{" "}
+                    <span className="font-bold">
+                        {timeRemaining.toString().slice(0, 4)}
+                    </span>{" "}
+                    seconds.
+                </div>
+                <div className="flex justify-evenly mt-4">
+                    <button
+                        onClick={() => setShowModal(false)}
+                        className="rounded-md font-medium  px-4 py-1 border"
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        onClick={() => {
+                            setStart(true);
+                            setIsStarted(false);
+                            setTimeRemaining(0);
+                            setIsTimerRunning(false);
+                            setShowModal(false);
+                        }}
+                        className="rounded-md font-medium bg-green-700 text-white px-4 py-1 border"
+                    >
+                        Play again
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
